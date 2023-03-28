9186d253f5de9635e89fef8427d5fe71db69ca5f
Nick Paternoster <70091428+PatJam21@users.noreply.github.com>
2023-03-28 09:44:17 -0500

    Correct file

--- data_formatter.py
+++ data_formatter.py
@@ -5,21 +5,22 @@ import numpy as np
 # Import data from csv files 
 training_dataset = pd.read_csv("Training_dataset.csv")
 testing_dataset = pd.read_csv("Testing_dataset.csv")
+full_dataset = pd.read_csv("Full Wine Data.csv")
 
 # Name of wine 
 train_type_key = training_dataset.iloc[:, 0].to_numpy()
 test_type_key = testing_dataset.iloc[:, 0].to_numpy()
-generic_type_key = np.array([])
+full_type_key = full_dataset.iloc[:, 0].to_numpy()
 
 # Grade classification of wine 
 train_class_key = training_dataset.iloc[:, 1].to_numpy()
 test_class_key = testing_dataset.iloc[:, 1].to_numpy()
-generic_class_key = np.array([])
+full_class_key = full_dataset.iloc[:, 1].to_numpy()
 
 # Attributes 
 train_value_key = training_dataset.iloc[:, 2:].to_numpy()
 test_value_key = testing_dataset.iloc[:, 2:].to_numpy()
-generic_value_key = np.array([])
+full_value_key = full_dataset.iloc[:, 2:].to_numpy()
 
 # Object to store all values of a datapoint 
 class DataPoint:
@@ -35,9 +36,13 @@ class DataSet:
         self.traindataset = np.array([])
         self.testdataset = np.array([])
 
+class SingleDataSet:
+    def __init__(self):
+        self.data = np.array([])
+
 class CFVDataSets: 
     def __init__(self):
-        self.group = DataSet()
+        self.group = SingleDataSet()
 
 # Populate data object
 def StructData(DataSet):
@@ -66,6 +71,26 @@ def StructData(DataSet):
 
     return DataSet
 
+
+# Populate data object
+def StructAllData(DataSet):
+    # set empty array 
+    dataset = []
+
+    # for the number of elements in the file, set the type, key, and values of each point and populate said point into testdataset. Testdataset is an array of objects. 
+    for i in range(len(full_type_key)): 
+        d = DataPoint()
+        d.type = full_type_key[i]
+        d.key = full_class_key[i]
+        d.values = full_value_key[i]
+        dataset.append(d)
+
+    #Sets the arrays within the DataSet object to the newly populated arrays 
+    DataSet.data = np.array(dataset)
+
+    return DataSet
+
+
 def GetData():
     #Create new dataset object 
     Dataset = DataSet()
@@ -75,17 +100,17 @@ def GetData():
 
 def Get_CFV_Data():
     #Create new dataset object 
-    Dataset = DataSet()
+    Dataset = SingleDataSet()
     CFV = CFVDataSets()
     #Format the data 
-    FormattedData = StructData(Dataset)
-    np.random.shuffle(FormattedData.traindataset)
+    FormattedData = StructAllData(Dataset)
+    np.random.shuffle(FormattedData.data)
 
-    return FormattedData.traindataset, CFV, FormattedData.testdataset
+    return FormattedData, CFV
 
 def Five_CFV_Split(iteration, dataset,cfv):
 # Split train and test datasets into 5 groups
-    groups = np.array_split(dataset, 5)
+    groups = np.array_split(dataset.data, 5)
     data = DataSet()
 
     for fold in range(5):
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ sigmoid = svm.SVC(kernel='sigmoid', C = 1)
 
 def main():
 
-    userinput = int(input("Crossfold validation or not (1 or 0): "))
+    userinput = int(input("Would you like to use Cross fold validation (1 or 0): "))
 
     if(userinput == 0):
         #Grab the dataset input 
@@ -76,16 +76,16 @@ def main():
             svm_overall_false_negative += fn
             svm_overall_false_positive += fp
 
-            knn_metrics = [a.ModelMeasures(dataset.group, k.KNN(dataset.group, k_value+1)) for k_value in range(20)]
+            knn_metrics = [a.ModelMeasures(dataset.group, k.KNN(dataset.group, k_value+1)) for k_value in range(5)]
             knn_accuracy, knn_sensitivity, knn_specificity, true_positive, true_negative, false_negative, false_positive = map(sum, zip(*knn_metrics))
             print("\nKNN")
-            print("Accuracy:", knn_accuracy/20)
-            print("Sensitivity:", knn_sensitivity/20)
-            print("Specificity:", knn_specificity/20)
-            print("True Positive:", true_positive/20)
-            print("True Negative:", true_negative/20)
-            print("False Negative:", false_negative/20)
-            print("False Positive:", false_positive/20)
+            print("Accuracy:", knn_accuracy/5)
+            print("Sensitivity:", knn_sensitivity/5)
+            print("Specificity:", knn_specificity/5)
+            print("True Positive:", true_positive/5)
+            print("True Negative:", true_negative/5)
+            print("False Negative:", false_negative/5)
+            print("False Positive:", false_positive/5)
 
             knn_overall_accuracy += knn_accuracy
             knn_overall_sensitivity += knn_sensitivity
@@ -104,13 +104,13 @@ def main():
         print("False Positive:", svm_overall_false_positive/5)
 
         print("KNN")
-        print("Accuracy:", knn_overall_accuracy/100)
-        print("Sensitivity:", knn_overall_sensitivity/100)
-        print("Specificity:", knn_overall_specificity/100)
-        print("True Positive:", knn_overall_true_positive/100)
-        print("True Negative:", knn_overall_true_negative/100)
-        print("False Negative:", knn_overall_false_negative/100)
-        print("False Positive:", knn_overall_false_positive/100)
+        print("Accuracy:", knn_overall_accuracy/25)
+        print("Sensitivity:", knn_overall_sensitivity/25)
+        print("Specificity:", knn_overall_specificity/25)
+        print("True Positive:", knn_overall_true_positive/25)
+        print("True Negative:", knn_overall_true_negative/25)
+        print("False Negative:", knn_overall_false_negative/25)
+        print("False Positive:", knn_overall_false_positive/25)
 
 
 
